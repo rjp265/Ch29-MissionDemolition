8faea16e772aa41c0b517c1a01331d601a32d88d
Robert Perez <49209439+rjp265@users.noreply.github.com>
2019-10-04 03:00:42 -0400

    Slingshot shoots

--- Assets/__Scripts/FollowCam.cs
+++ Assets/__Scripts/FollowCam.cs
@@ -1,67 +1,69 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class FollowCam : MonoBehaviour
-{
-    static public FollowCam S; // a FollowCam Singleton
 
-    // Fields set in the Unity Inspector pane
+
+public class FollowCam : MonoBehaviour {
+    static public GameObject POI; // The static point of interest
+
+    [Header("Set in Inspector")]
     public float easing = 0.05f;
-    public Vector2 minXY;
-    public bool _______;
-    // Fields set in dynamically
-    public GameObject poi; // The point of interest
-    public float camZ; // The desired Z pos of the camera
+    public Vector2 minXY = Vector2.zero;
 
-    void Awake()
+    [Header("Set Dynamically")]
+    public float camZ; // The desired z pos of the camera
+
+    private void Awake()
     {
-        S = this;
         camZ = this.transform.position.z;
     }
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         Vector3 destination;
-        if (poi == null)
+        // If there is no poi, return to P: [0,0,0]
+        if (POI == null)
         {
             destination = Vector3.zero;
         }
         else
         {
             // Get the position of the poi
-            destination = poi.transform.position;
-            // if poi is a Projectile, check to see if it's at rest
-            if (poi.tag == "Projectile")
+            destination = POI.transform.position;
+            // If poi is a Projectile, check to see if it's at rest
+            if (POI.tag == "Projectile")
             {
-                // If it is sleeping (that is, not moving)
-                if (poi.GetComponent<Rigidbody>().IsSleeping())
+                // if it is sleeping (that is, not moving)
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
                 {
                     // return to default view
-                    poi = null;
-                    Destroy(poi);
-                    MissionDemolition.SwitchView("Both");
                     // in the next update
+                    POI = null;
                     return;
                 }
             }
         }
-        // Limit the X & Y to minimum values
+
+
+        // Limit the X & Y to Max values
+        // the slingshot starts in -x, -y territory, so don't
+        // start moving until the projectile gets
+        // past the 0,0 point of the world
+
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
+
         // Interpolate from the current Camera position toward destination
         destination = Vector3.Lerp(transform.position, destination, easing);
-        // Retain a destination.z of camZ
+
+        // Force destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
+
         // Set the camera to the destination
-        this.transform.position = destination;
+        transform.position = destination;
+
         // Set the orthographicSize of the Camera to keep Ground in view
-        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
