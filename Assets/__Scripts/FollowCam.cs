417bce0d18dab1fb3f5b10cca635a0d7f0b242d2
Robert Perez <49209439+rjp265@users.noreply.github.com>
2019-10-04 03:24:40 -0400

    We doin clouds now

--- Assets/__Scripts/FollowCam.cs
+++ Assets/__Scripts/FollowCam.cs
@@ -4,6 +4,8 @@ public class FollowCam : MonoBehaviour
 {
     static public FollowCam S; // a FollowCam Singleton
                                // fields set in the Unity Inspector pane
+    public float easing = 0.05f;
+    public Vector2 minXY;
     public bool _____________________________;
     // fields set dynamically
     public GameObject poi; // The point of interest
@@ -19,9 +21,16 @@ public class FollowCam : MonoBehaviour
         if (poi == null) return; // return if there is no poi
                                  // Get the position of the poi
         Vector3 destination = poi.transform.position;
+        // Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         // Retain a destination.z of camZ
         destination.z = camZ;
         // Set the camera to the destination
         transform.position = destination;
+        // Set the orthographicSize of the Camera to keep Ground in view
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
     }
 }
