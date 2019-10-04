cb059177c1b2e0d2fdf80b04ec8b8ae3a90b7e55
Robert Perez <49209439+rjp265@users.noreply.github.com>
2019-10-04 03:55:48 -0400

    Castle done

--- Assets/__Scripts/CloudCrafter.cs
+++ Assets/__Scripts/CloudCrafter.cs
@@ -1,30 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CloudCrafter : MonoBehaviour {
-    [Header("Set in Inspector")]
+﻿using UnityEngine;
+using System.Collections;
+public class CloudCrafter : MonoBehaviour
+{
+    // fields set in the Unity Inspector pane
     public int numClouds = 40; // The # of clouds to make
-    public GameObject cloudPrefab; // The prefab for the clouds
-    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
-    public Vector3 cloudPosMax = new Vector3(150, 100, 10);
+    public GameObject[] cloudPrefabs; // The prefabs for the clouds
+    public Vector3 cloudPosMin; // Min position of each cloud
+    public Vector3 cloudPosMax; // Max position of each cloud
     public float cloudScaleMin = 1; // Min scale of each cloud
-    public float cloudScaleMax = 3; //Max scale of each cloud
+    public float cloudScaleMax = 5; // Max scale of each cloud
     public float cloudSpeedMult = 0.5f; // Adjusts speed of clouds
-    private GameObject[] cloudInstances;
-
+    public bool _____________________________;
+    // fields set dynamically
+    public GameObject[] cloudInstances;
     void Awake()
     {
-        // Make an array large enough to hold all the Cloud_ intances
+        // Make an array large enough to hold all the Cloud_ instances
         cloudInstances = new GameObject[numClouds];
         // Find the CloudAnchor parent GameObject
         GameObject anchor = GameObject.Find("CloudAnchor");
-        //Iterate through and make Cloud_#
+        // Iterate through and make Cloud_s
         GameObject cloud;
-        for (int i=0; i<numClouds; i++)
+        for (int i = 0; i < numClouds; i++)
         {
-            // Make an instance of cloudPrefab
-            cloud = Instantiate<GameObject>(cloudPrefab);
+            // Pick an int between 0 and cloudPrefabs.Length-1
+            // Random.Range will not ever pick as high as the top number
+            int prefabNum = Random.Range(0, cloudPrefabs.Length);
+            // Make an instance
+            cloud = Instantiate(cloudPrefabs[prefabNum]) as GameObject;
             // Position cloud
             Vector3 cPos = Vector3.zero;
             cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
@@ -32,7 +35,7 @@ public class CloudCrafter : MonoBehaviour {
             // Scale cloud
             float scaleU = Random.value;
             float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
-            // Smaller clouds (with smaller scaleU) should be nearer the ground
+            // Smaller clouds (with smaller scaleU) should be nearer the ground)
             cPos.y = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
             // Smaller clouds should be further away
             cPos.z = 100 - 90 * scaleU;
@@ -40,17 +43,15 @@ public class CloudCrafter : MonoBehaviour {
             cloud.transform.position = cPos;
             cloud.transform.localScale = Vector3.one * scaleVal;
             // Make cloud a child of the anchor
-            cloud.transform.SetParent(anchor.transform);
+            cloud.transform.parent = anchor.transform;
             // Add the cloud to cloudInstances
             cloudInstances[i] = cloud;
         }
     }
-	
-	// Update is called once per frame
-	void Update()
+    void Update()
     {
         // Iterate over each cloud that was created
-        foreach(GameObject cloud in cloudInstances)
+        foreach (GameObject cloud in cloudInstances)
         {
             // Get the cloud scale and position
             float scaleVal = cloud.transform.localScale.x;
@@ -58,7 +59,7 @@ public class CloudCrafter : MonoBehaviour {
             // Move larger clouds faster
             cPos.x -= scaleVal * Time.deltaTime * cloudSpeedMult;
             // If a cloud has moved too far to the left...
-            if(cPos.x <= cloudPosMin.x)
+            if (cPos.x <= cloudPosMin.x)
             {
                 // Move it to the far right
                 cPos.x = cloudPosMax.x;
